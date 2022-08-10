98dd9d150902e61171a707667239d3ca8176313b
sean.zhao <zhaoxiang2845@gmail.com>
2022-08-10 15:50:42 +0800

    change cache-control to no-cache

--- api/index.ts
+++ api/index.ts
@@ -22,10 +22,7 @@ export default async function handler(
     const file = await getScreenshot(html, "png", isDev);
     res.statusCode = 200;
     res.setHeader("Content-Type", `image/${"png"}`);
-    res.setHeader(
-      "Cache-Control",
-      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
-    );
+    res.setHeader("Cache-Control", `no-cache`);
     res.end(file);
   } catch (e) {
     res.statusCode = 500;
