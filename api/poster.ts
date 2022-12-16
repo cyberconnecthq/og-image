03ebc4db66d873fd9bf26d6aed834ac181890f7d
Sean Zhao <zhaoxiang2845@gmail.com>
2022-12-16 17:21:20 +0800

    fix: query debug

--- api/poster.ts
+++ api/poster.ts
@@ -23,7 +23,7 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
     html = await getPoster(parsedReq as PosterRequest);
 
     if (isQueryDebug) {
-      return res.send(query);
+      return res.end(query);
     }
 
     if (isHtmlDebug) {
